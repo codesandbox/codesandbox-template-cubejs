e8ac5e767883895590bbd83cc066471c309226d9
Tamas Szuromi <tromika@gmail.com>
2022-08-01 22:45:44 +0200

    Create a cubejs example (#1)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cube.js React dashboard Example
+
+This is a [Cube.js](https://cube.dev/) React dashboard example built on a Cube.js Server and Postgresql.
+
+[Configuration](https://codesandbox.io/docs/projects/learn/setting-up/tasks) has been added to optimize it for [CodeSandbox Projects](https://codesandbox.io/p/dashboard).
+
+[![Edit in CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/github/codesandbox/codesandbox-template-cubejs)
+
+## Resources
+
+- [CodeSandbox Projects — Docs](https://codesandbox.io/docs/projects)
+- [CodeSandbox — Discord](https://discord.gg/Ggarp3pX5H)
+- [Cubejs — Docs](https://cube.dev/docs)
---
+++ packages/dashboard-app/src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { withRouter } from "react-router";
+import { Layout } from "antd";
+import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
+import { ApolloProvider } from "react-apollo";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import client from "./graphql/client";
+
+import Header from './components/Header';
+// import aws_exports from './aws-exports';
+
+const token = process.env.REACT_APP_CUBEJS_TOKEN
+
+const cubejsApi = cubejs(token, {
+  apiUrl: process.env.REACT_APP_API_URL
+});
+
+// Amplify.configure(aws_exports);
+
+//const client = new AWSAppSyncClient(
+//  {
+//    disableOffline: true,
+//    url: aws_exports.aws_appsync_graphqlEndpoint,
+//    region: aws_exports.aws_appsync_region,
+//    auth: {
+//      type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+//      jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken()
+//    },
+//  },
+//  { cache: new InMemoryCache() }
+//);
+
+// Hub.listen('auth', (data) => {
+//   if (data.payload.event === 'signOut') {
+//     client.resetStore();
+//   }
+// });
+
+const AppLayout = ({ location, children }) => (
+  <Layout style={{ height: "100%" }}>
+    <Header location={location} />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>
+);
+
+const App = withRouter(({ location, children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <ApolloHooksProvider client={client}>
+          <AppLayout location={location}>{children}</AppLayout>
+      </ApolloHooksProvider>
+    </ApolloProvider>
+  </CubeProvider>
+));
+
+export default App;
